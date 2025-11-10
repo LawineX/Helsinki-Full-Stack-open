c2174eedeb75ded42e1e1b8ee71eee1d994535be
lawine <854340647@qq.com>
2025-11-10 14:14:31 +0800

    feat Part3 3.16练习完成

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 3001;
 app.use(express.json());
 
 const addressBook = [
@@ -28,4 +27,60 @@ const addressBook = [
 
 app.get("/api/persons", (req, res) => {
   res.json(addressBook);
+});
+
+app.get("/info", (req, res) => {
+  const entryCount = addressBook.length;
+  const currentTime = new Date();
+  res.send(
+    `<p>Phonebook has info for <strong>${entryCount}</strong> people</p><p>${currentTime}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const person = addressBook.find((p) => p.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const index = addressBook.findIndex((p) => p.id === id);
+  if (index !== -1) {
+    addressBook.splice(index, 1);
+    // addressBook = addressBook.filter((p) => p.id !== id);
+    res.send("Person deleted");
+    res.status(204).end();
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.post("/api/persons", (req, res) => {
+  const checkNameExists = () =>
+    addressBook.find((p) => p.name === req.body.name);
+  const checkBodyExists = () => !req.body.name || !req.body.number;
+
+  if (checkBodyExists()) {
+    return res.status(400).json({ error: "name or number is missing" });
+  }
+  if (checkNameExists()) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+  const generateId = () => {
+    return Math.floor(Math.random() * 10000).toString();
+  };
+  const { name, number } = req.body;
+  const newPerson = { id: generateId(), name, number };
+  addressBook.push(newPerson);
+  res.status(201).json(newPerson);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
